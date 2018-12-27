4016dc1f0e7ec2adbdd22e2a30db06fbfc414e0d
davidjeddy <me@davidjeddy.com>
2018-12-27 17:10:30 -0500

    UPDATED ubuntu_ec2 runbook script.

--- docs/runbooks/ubuntu_ec2_setup.sh
+++ docs/runbooks/ubuntu_ec2_setup.sh
@@ -6,11 +6,12 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt purge -y libssl-dev
 sudo apt install -y htop vim
-sudo apt install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev sqlite3 nodejs
-sudo apt install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
+sudo apt install -y autoconf bison build-essential libyaml-dev  zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev sqlite3 nodejs
 
+echo "Must use SSL 1.0-dev with Ruby 2.3.x"
+sudo apt install -y libssl1.0-dev libreadline-dev
 
-echo "Instaling ebenv"
+echo "Instaling rbenv"
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
@@ -18,7 +19,7 @@ source ~/.bashrc
 type rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 rbenv install -l
-rbenv install 2.3.8
+rbenv install -f 2.3.8
 rbenv global 2.3.8
 
 
