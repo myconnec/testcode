5e3046dc4a79b9574b968ddd91ac7a95862b8205
davidjeddy <me@davidjeddy.com>
2019-01-10 18:46:34 -0500

    ADDED IRB console to layout VW, whitelisted docker 172.17.0.0/16 network.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.web_console.whitelisted_ips = '172.17.0.0/16'
+
   end
 end
