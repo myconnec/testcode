4c5ea76cad342fcea125b737cab1379d3041c195
davidjeddy <me@davidjeddy.com>
2019-01-14 20:09:27 -0500

    ADDED indenting to CNTL fn()s

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,14 +1,14 @@
 class SubcategoriesController < ApplicationController
 
     def show
-    @listings = Listing.where(subcategory_id: params[:id]).order("created_at DESC")
-    @category = Category.find(params[:category_id])
-    @subcategory = Subcategory.find(params[:id])
+        @listings = Listing.where(subcategory_id: params[:id]).order("created_at DESC")
+        @category = Category.find(params[:category_id])
+        @subcategory = Subcategory.find(params[:id])
     end
       
     def find_by_category
-    category = Category.find(params[:category_id])
-    subcategories = category.subcategories.find_all
-    render json: { subcategories: subcategories }
+        category = Category.find(params[:category_id])
+        subcategories = category.subcategories.find_all
+        render json: { subcategories: subcategories }
     end
 end
