1db7dfab04e12f38f4fa546faab430fe08f2cf8f
davidjeddy <me@davidjeddy.com>
2019-02-10 11:58:22 -0500

    ADDED default / not found profile image.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   
   validates_uniqueness_of :username
   
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "150x150>" }, default_url: "no-photo-available.jpg"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "150x150>" }, default_url: "no-photo-available.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   
 end
