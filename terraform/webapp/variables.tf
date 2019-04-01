de3770c6b01efac5088d3f51beb59f058e7e06b1
davidjeddy <me@davidjeddy.com>
2019-04-01 19:44:25 -0400

    MOVED TF logic under ./terraform. MOVED Ansible logic under ./ansible. DELETE ./docs incl the runbooks as they are no longer needed.

---
+++ terraform/webapp/variables.tf
@@ -0,0 +1,11 @@
+# These values should be populated from the .env using the follow command:
+# terraform [comman] [options]] -var-file=.env
+
+variable "APP_ENV" {}
+variable "APP_NAME" {}
+variable "AWS_REGION" {}
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_PEM_KEY_PAIR" {}
+variable "DB_USER" {}
+variable "DB_PASS" {}
