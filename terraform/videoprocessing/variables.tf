eb77edce3add88199154a6c2ce2a7f420f4e4616
davidjeddy <me@davidjeddy.com>
2019-04-02 23:13:45 -0400

    Buckets, Transcoder pipeline and presets create. Transcoder role policy needs fixing, sts:AssumeRole is not being attached.

---
+++ terraform/videoprocessing/variables.tf
@@ -0,0 +1,6 @@
+# These values should be populated from the .env using the follow command:
+# terraform [comman] [options]] -var-file=.env
+
+variable "APP_ENV" {}
+variable "APP_NAME" {}
+variable "AWS_REGION" {}
