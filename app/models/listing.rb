e03ae15e1c88fd3e80b3772fdf10b20f154fc1d1
davidjeddy <me@davidjeddy.com>
2019-04-25 20:00:15 -0400

    Now writing source media into a non-public folder that is a S3 bucket mount.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,29 +1,42 @@
 class Listing < ActiveRecord::Base
+  acts_as_votable
+  is_impressionable
+
+  after_validation :geocode
+
   belongs_to :category
   belongs_to :subcategory
   belongs_to :user
-  acts_as_votable
-  is_impressionable
   
-  
+  geocoded_by :full_address
+
   validates_presence_of :user_id
+  validates_presence_of :price
+  validates_presence_of :category_id
+  validates_presence_of :subcategory_id
   validates_presence_of :title
-  validates_presence_of :description
   validates_presence_of :city
   validates_presence_of :state
-  validates_presence_of :price
+  validates_presence_of :zipcode
+  validates_presence_of :pin_image
+  validates_presence_of :description   
  
-
-    
-  geocoded_by :full_address
-  after_validation :geocode
-  
   has_many :comments, dependent: :destroy
-  has_attached_file :pin_image
-  validates_attachment_size :pin_image, :less_than => 800.megabytes
-  validates_attachment_presence :pin_image
-  validates_attachment_content_type :pin_image, :content_type => ["video/quicktime", "video/x-ms-wmv", "video/mp4", "video/3gpp", "audio/mp4", "video/x-flv", "video/x-msvideo", "video/webm"]
+  # place the source media at this location
+  has_attached_file :pin_image, :path => ":rails_root/"+ENV["AWS_S3_MEDIA_SOURCE_BUCKET"]+"-"+ENV["APP_ENV"]+"/:class/:attachment/:id_partition/:filename"
 
+  # validates_attachment_size :pin_image, :less_than => 800.megabytes
+  validates_attachment_presence :pin_image
+  validates_attachment_content_type :pin_image, :content_type => [
+    "video/quicktime",
+    "video/x-ms-wmv",
+    "video/mp4",
+    "video/3gpp",
+    "audio/mp4",
+    "video/x-flv",
+    "video/x-msvideo",
+    "video/webm"
+  ]
  
   def full_address
     [city, state, zipcode].join(', ')
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190122015640) do
+ActiveRecord::Schema.define(version: 20190425235344) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", limit: 255
@@ -25,19 +25,16 @@ ActiveRecord::Schema.define(version: 20190122015640) do
     t.datetime "updated_at",               null: false
   end
 
-  add_index "comments", ["listing_id"], name: "index_comments_on_listing_id", using: :btree
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
-
   create_table "impressions", force: :cascade do |t|
-    t.string   "impressionable_type", limit: 255
+    t.string   "impressionable_type", limit: 32
     t.integer  "impressionable_id",   limit: 4
     t.integer  "user_id",             limit: 4
-    t.string   "controller_name",     limit: 255
-    t.string   "action_name",         limit: 255
-    t.string   "view_name",           limit: 255
-    t.string   "request_hash",        limit: 255
-    t.string   "ip_address",          limit: 255
-    t.string   "session_hash",        limit: 255
+    t.string   "controller_name",     limit: 32
+    t.string   "action_name",         limit: 32
+    t.string   "view_name",           limit: 32
+    t.string   "request_hash",        limit: 256
+    t.string   "ip_address",          limit: 32
+    t.string   "session_hash",        limit: 32
     t.text     "message",             limit: 65535
     t.text     "referrer",            limit: 65535
     t.text     "params",              limit: 65535
@@ -45,26 +42,16 @@ ActiveRecord::Schema.define(version: 20190122015640) do
     t.datetime "updated_at"
   end
 
-  add_index "impressions", ["controller_name", "action_name", "ip_address"], name: "controlleraction_ip_index", using: :btree
-  add_index "impressions", ["controller_name", "action_name", "request_hash"], name: "controlleraction_request_index", using: :btree
-  add_index "impressions", ["controller_name", "action_name", "session_hash"], name: "controlleraction_session_index", using: :btree
-  add_index "impressions", ["impressionable_type", "impressionable_id", "ip_address"], name: "poly_ip_index", using: :btree
-  add_index "impressions", ["impressionable_type", "impressionable_id", "params"], name: "poly_params_request_index", length: {"impressionable_type"=>nil, "impressionable_id"=>nil, "params"=>255}, using: :btree
-  add_index "impressions", ["impressionable_type", "impressionable_id", "request_hash"], name: "poly_request_index", using: :btree
-  add_index "impressions", ["impressionable_type", "impressionable_id", "session_hash"], name: "poly_session_index", using: :btree
-  add_index "impressions", ["impressionable_type", "message", "impressionable_id"], name: "impressionable_type_message_index", length: {"impressionable_type"=>nil, "message"=>255, "impressionable_id"=>nil}, using: :btree
-  add_index "impressions", ["user_id"], name: "index_impressions_on_user_id", using: :btree
-
   create_table "listings", force: :cascade do |t|
-    t.string   "title",                  limit: 255
+    t.string   "title",                  limit: 128
     t.text     "description",            limit: 65535
-    t.string   "city",                   limit: 255
-    t.string   "state",                  limit: 255
-    t.string   "zipcode",                limit: 255
-    t.string   "price",                  limit: 255
-    t.string   "address1",               limit: 255
-    t.string   "address2",               limit: 255
-    t.string   "ademail",                limit: 255
+    t.string   "city",                   limit: 32
+    t.string   "state",                  limit: 32
+    t.string   "zipcode",                limit: 32
+    t.string   "price",                  limit: 32
+    t.string   "address1",               limit: 32
+    t.string   "address2",               limit: 32
+    t.string   "ademail",                limit: 32
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id",            limit: 4
@@ -72,74 +59,63 @@ ActiveRecord::Schema.define(version: 20190122015640) do
     t.float    "latitude",               limit: 24
     t.float    "longitude",              limit: 24
     t.integer  "user_id",                limit: 4
-    t.string   "pin_image_file_name",    limit: 255
-    t.string   "pin_image_content_type", limit: 255
+    t.string   "media_filename",         limit: 32
+    t.string   "pin_image_content_type", limit: 32
     t.integer  "pin_image_file_size",    limit: 4
     t.datetime "pin_image_updated_at"
   end
 
-  add_index "listings", ["user_id"], name: "index_listings_on_user_id", using: :btree
-
   create_table "models", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  limit: 1024, default: "", null: false
+    t.string   "encrypted_password",     limit: 1024, default: "", null: false
+    t.string   "reset_password_token",   limit: 1024
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,    default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.string   "current_sign_in_ip",     limit: 32
+    t.string   "last_sign_in_ip",        limit: 32
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
   end
 
-  add_index "models", ["email"], name: "index_models_on_email", unique: true, using: :btree
-  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
-
   create_table "subcategories", force: :cascade do |t|
-    t.string  "name",        limit: 255
+    t.string  "name",        limit: 32
     t.integer "category_id", limit: 4
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  limit: 1024, default: "", null: false
+    t.string   "encrypted_password",     limit: 1024, default: "", null: false
+    t.string   "reset_password_token",   limit: 1024
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,    default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.string   "username",               limit: 255
-    t.string   "bio",                    limit: 255
-    t.string   "avatar_file_name",       limit: 255
-    t.string   "avatar_content_type",    limit: 255
+    t.string   "current_sign_in_ip",     limit: 32
+    t.string   "last_sign_in_ip",        limit: 32
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
+    t.string   "username",               limit: 32
+    t.string   "bio",                    limit: 1024
+    t.string   "avatar_file_name",       limit: 1024
+    t.string   "avatar_content_type",    limit: 32
     t.integer  "avatar_file_size",       limit: 4
     t.datetime "avatar_updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
   create_table "votes", force: :cascade do |t|
     t.integer  "votable_id",   limit: 4
-    t.string   "votable_type", limit: 255
+    t.string   "votable_type", limit: 32
     t.integer  "voter_id",     limit: 4
-    t.string   "voter_type",   limit: 255
+    t.string   "voter_type",   limit: 32
     t.boolean  "vote_flag"
-    t.string   "vote_scope",   limit: 255
+    t.string   "vote_scope",   limit: 32
     t.integer  "vote_weight",  limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
-  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
 
 end
