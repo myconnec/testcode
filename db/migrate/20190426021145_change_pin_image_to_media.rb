d495db355737def9e2f6eddedd97cbd69e7fd3e5
davidjeddy <me@davidjeddy.com>
2019-04-25 22:22:49 -0400

    FIXED _filename error, should stay _file_name.

--- db/migrate/20190426021145_change_pin_image_to_media.rb
+++ db/migrate/20190426021145_change_pin_image_to_media.rb
@@ -1,13 +1,13 @@
 class ChangePinImageToMedia < ActiveRecord::Migration
   def change
-    rename_column :listings, :pin_image_file_name, :media_filename
+    rename_column :listings, :pin_image_file_name, :media_file_name
     rename_column :listings, :pin_image_content_type, :media_content_type
     rename_column :listings, :pin_image_file_size, :media_file_size
     rename_column :listings, :pin_image_updated_at, :media_updated_at
   end
 
   def down
-    rename_column :listings, :media_filename, :pin_image_file_name
+    rename_column :listings, :media_file_name, :pin_image_file_name
     rename_column :listings, :media_content_type, :pin_image_content_type
     rename_column :listings, :media_file_size, :pin_image_file_size
     rename_column :listings, :media_updated_at, :pin_image_updated_at
