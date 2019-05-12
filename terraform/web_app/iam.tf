946eabf3a274a1c03579b426b6125e3a6547c18c
davidjeddy <me@davidjeddy.com>
2019-05-11 20:01:34 -0400

    ADDED logic to generate a IAM user w/ a policy to allow access to the S3 media_* buckets.

--- terraform/web_app/iam.tf
+++ terraform/web_app/iam.tf
@@ -9,7 +9,7 @@ resource "aws_iam_role_policy_attachment" "code_commit_read_only" {
 
 # S3
 
-resource "aws_iam_role_policy_attachment" "s3_read_only" {
+resource "aws_iam_role_policy_attachment" "s3_full_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
   role       = "${aws_iam_role.ec2_web_server_role.name}"
 }
@@ -19,4 +19,38 @@ resource "aws_iam_role_policy_attachment" "s3_read_only" {
 resource "aws_iam_role" "ec2_web_server_role" {
   assume_role_policy = "${file("./terraform/web_app/policies/assumerolepolicy.json")}"
   name               = "CHServiceRoleForEC2WithCodeCommitReadOnlyPermission"
+}
+
+resource "aws_iam_user_policy" "s3_bucket_access_policy" {
+  name = "${var.APP_NAME}_s3_bucket_access_policy_${var.APP_ENV}"
+  user = "${aws_iam_user.s3_bucket_access_user.name}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:*"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.media_display_bucket_id}",
+                "arn:aws:s3:::${var.media_display_bucket_id}/*",
+                "arn:aws:s3:::${var.media_source_bucket_id}",
+                "arn:aws:s3:::${var.media_source_bucket_id}*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_user" "s3_bucket_access_user" {
+  name = "${var.APP_NAME}_s3_bucket_access_user_${var.APP_ENV}"
+  path = "/"
+}
+
+resource "aws_iam_access_key" "s3_bucket_access_key" {
+  user = "${aws_iam_user.s3_bucket_access_user.name}"
 }
