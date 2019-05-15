046f9ae3f5f2d59aea97022fe46664e1b38821c0
davidjeddy <me@davidjeddy.com>
2019-05-15 19:38:12 -0400

    UPDATED listings.js to better interact with the user and display values as expected.

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,6 +1,11 @@
 var getSubcategories = function(category_id){
   var $subcategories = $('#listing_subcategory_id');
-  
+
+  if (isNaN(parseInt(category_id))) {
+    $subcategories.empty();
+    return;
+  }
+
   $.get('/subcategories/find_by_category', { category_id: category_id }, function(data){
     $subcategories.empty();
     $.each(data.subcategories, function(index, subcategory){
@@ -8,16 +13,11 @@ var getSubcategories = function(category_id){
       option.attr('value', subcategory.id);
       option.text(subcategory.name);
       $subcategories.append(option)
-    });   
+    });
   })
 };
 
 // on change of Category DDL, trigger Sub-category population
 $('#listing_category_id').on('change', function(){
   getSubcategories($('#listing_category_id').val());
-});
-
-$(document).ready(function(){
- // get list for initiallty  selected Category item
- getSubcategories(1);
 });
