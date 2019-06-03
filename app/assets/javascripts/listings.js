609fc559b9b39d1889732fa10b30a4545a65c662
davidjeddy <me@davidjeddy.com>
2019-06-03 13:03:24 -0400

    UPDATED application.html.erb formatting. Added line breaks between content and header / footer menus bars. MOVED amount field in Post create / edit form below category & sub-category fields. ADDED basic JS logic to set cost field to 0.00 and disable to fields for community posts."

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -24,12 +24,23 @@ $('#listing_category_id').on('change', function(){
 });
 
 // remove element on any change of the category DDL
-$('#listing_category_id, #listing_category_id').on('change', function(){
+$('#listing_category_id, #listing_subcategory_id').on('change', function(){
   console.log('changed0');
+  $('#listing_price').val('').attr('disabled', false);
   $('#payment_form').addClass('hidden');
+
+  // community category posts do not cost anything, set price to 0.00 and disable the field
+  if ($('#listing_category_id').val() == '2') {
+    $('#listing_price').val('0.00').attr('disabled', true);
+  }
 })
 
 $('#listing_subcategory_id').on('change', function(){
+  // if 'community' category is selected, no payment required.
+  if ($('#listing_category_id').val() == '2') {
+    return true;
+  }
+
   if ($('#listing_subcategory_id').val() > 10) {
     console.log('toggle1');
     $('#payment_form').removeClass('hidden');
