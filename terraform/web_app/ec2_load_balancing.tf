5407d077ca10bee0c8fd6fd7fd3fa8886f302cc4
davidjeddy <me@davidjeddy.com>
2019-06-09 13:02:18 -0400

    Mid-step stop.

--- terraform/web_app/ec2_load_balancing.tf
+++ terraform/web_app/ec2_load_balancing.tf
@@ -33,23 +33,3 @@ resource "aws_lb" "web_app" {
     "${data.aws_subnet.web_app.*.id}"
   ]
 }
-
-resource "aws_lb_listener" "web_app" {
-  load_balancer_arn = "${aws_lb.web_app.arn}"
-  # port              = "443"
-  # protocol          = "HTTPS"
-  # ssl_policy        = "ELBSecurityPolicy-2016-08"
-  # certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.web_app.arn}"
-  }
-}
-
-resource "aws_lb_target_group" "web_App" {
-  name     = "Web App"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = "${aws_default_vpc.default.id}"
-}
