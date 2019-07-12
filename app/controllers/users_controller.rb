c477b736fdfa47d137765e3d0e2f29ce590763e1
davidjeddy <me@davidjeddy.com>
2019-07-12 16:31:02 -0400

    Posts over 30 days no longer display in a users profile view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-
     def show
-      @user = User.find_by username: params[:username]  
+      @user = User.find_by username: params[:username]
+      @user.listings = Listing
+        .where(user_id: @user.id)
+        .where("ending_at > '#{Time.now.to_i}'")
+        .order("created_at DESC")
     end
-
 end
