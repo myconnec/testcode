9aa2d59b168d35672b6fe24b146f8451c0b693f4
davidjeddy <me@davidjeddy.com>
2019-07-19 16:51:08 -0400

    Cont moved file upload logic inside of JQ doc ready.

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -82,17 +82,16 @@ $( document ).ready(function() {
     console.log('Show loading spinner...')
     // $("#overlay").toggle()
   });
-});
 
-// Upload directly to S3
-$(function() {
   $('.directUpload').find("input:file").each(function(i, elem) {
     var fileInput = $(elem);
     var form = $(fileInput.parents('form:first'));
     var submitButton = form.find('input[type="submit"]');
     var progressBar  = $("<div class='bar'></div>");
     var barContainer = $("<div class='progress'></div>").append(progressBar);
-    fileInput.after(barContainer);
+    
+    $('#listing_media_info').after(barContainer);
+
     fileInput.fileupload({
         fileInput: fileInput,
         url: form.data('url'),
