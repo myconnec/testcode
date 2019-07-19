7782ff4b08bc121d4b418dd1c477497cfc2200fc
davidjeddy <me@davidjeddy.com>
2019-07-19 16:01:04 -0400

    Attempting to enable S3 upload with a full form.

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,42 +1,6 @@
 $( document ).ready(function() {
-  // Upload directly to S3
-  $('#new_listing, #listings_submit').on('submit', function (event){
-    console.log('Show loading spinner...')
-    event.preventDefault()
-    // $("#overlay").toggle()
-    return false
-  });
-
-  var myVideos = [];
 
   window.URL = window.URL || window.webkitURL;
-
-  $('#listing_media').on('change', function (){
-    files = this.files;
-    myVideos.push(files[0]);
-    var video = document.createElement('video');
-    video.preload = 'metadata';
-
-    video.onloadedmetadata = function() {
-      window.URL.revokeObjectURL(video.src);
-      var duration = video.duration;
-      myVideos[myVideos.length - 1].duration = duration;
-      updateInfos();
-    }
-
-    video.src = URL.createObjectURL(files[0]);;    
-  })
-
-  function updateInfos() {
-    var infos = document.getElementById('listing_media_info');
-    infos.textContent = "";
-    for (var i = 0; i < myVideos.length; i++) {
-      // infos.textContent += myVideos[i].name + " duration: " + myVideos[i].duration + '\n';
-      var minutes = Math.floor(myVideos[i].duration / 60); // 7
-      var seconds = Math.floor(myVideos[i].duration % 60); // 30
-      infos.textContent += "Run time: " + minutes + ' min. and ' + seconds +  ' secs.\n';
-    }
-  }
 
   var getSubcategories = function(category_id){
     var $subcategories = $('#listing_subcategory_id');
@@ -58,6 +22,30 @@ $( document ).ready(function() {
     })
   }
   
+  var myVideos = [];
+
+  // get the media  runtime and display to the user.
+  $('#listing_media').on('change', function (){
+    files = this.files;
+    myVideos.push(files[0]);
+    var video = document.createElement('video');
+    video.preload = 'metadata';
+
+    video.onloadedmetadata = function() {
+      var duration = video.duration;
+      myVideos[myVideos.length - 1].duration = duration;
+      var infos = document.getElementById('listing_media_info');
+      infos.textContent = "";
+      for (var i = 0; i < myVideos.length; i++) {
+        var minutes = Math.floor(myVideos[i].duration / 60); // 7
+        var seconds = Math.floor(myVideos[i].duration % 60); // 30
+        infos.textContent += "Run time: " + minutes + ' min. and ' + seconds +  ' secs.\n';
+      }
+    }
+
+    video.src = URL.createObjectURL(files[0]);
+  })
+
   // on change of Category DDL, trigger Sub-category population
   $('#listing_category_id').on('change', function(){
     getSubcategories($('#listing_category_id').val());
@@ -89,52 +77,60 @@ $( document ).ready(function() {
     }
   });
 
-  $('.directUpload').find("input:file").each(function(i, elem) {
-    var fileInput    = $(elem);
-    var form         = $(fileInput.parents('form:first'));
-    var submitButton = form.find('input[type="submit"]');
-    var progressBar  = $("<div class='bar'></div>");
-    var barContainer = $("<div class='progress'></div>").append(progressBar);
-    fileInput.after(barContainer);
-    fileInput.fileupload({
-        fileInput:  fileInput,
-        url:  form.data('url'),
-        type: 'POST',
-        autoUpload: true,
-        formData: form.data('form-data'),
-        paramName:  'file', // S3 does not like nested name fields i.e. name="user[avatar_url]"
-        dataType: 'XML',  // S3 returns XML if success_action_status is set to 201
-        replaceFileInput: false,
-        progressall: function (e, data) {
-            var progress = parseInt(data.loaded / data.total * 100, 10);
-            progressBar.css('width', progress + '%')
-        },
-        start: function (e) {
-            submitButton.prop('disabled', true);
-            progressBar.
-              css('background', 'green').
-              css('display', 'block').
-              css('width', '0%').
-              text("Loading...");
-        },
-        done: function(e, data) {
-            submitButton.prop('disabled', false);
-            progressBar.text("Uploading done");
+  $('#new_listing').on('submit', function (event){
+    event.preventDefault()
+    console.log('Show loading spinner...')
+    // $("#overlay").toggle()
 
-            // extract key and generate URL from response
-            var key   = $(data.jqXHR.responseXML).find("Key").text();
-            var url   = '//' + form.data('host') + '/' + key;
+    $('#listing_media').find("input:file").each(function(i, elem) {
+      console.log('listing_media...', i, elem)
 
-            // create hidden field
-            var input = $("<input />", { type:'input', name: fileInput.attr('name'), value: url })
-            form.append(input);
-        },
-        fail: function(e, data) {
-            submitButton.prop('disabled', false);
-            progressBar.
-              css("background", "red").
-              text("Failed");
-        }
+      var fileInput    = $(elem);
+      var form         = $(fileInput.parents('form:first'));
+      var submitButton = form.find('input[type="submit"]');
+      var progressBar  = $("<div class='bar'></div>");
+      var barContainer = $("<div class='progress'></div>").append(progressBar);
+      fileInput.after(barContainer);
+      fileInput.fileupload({
+          fileInput:  fileInput,
+          url:  form.data('url'),
+          type: 'POST',
+          autoUpload: true,
+          formData: form.data('form-data'),
+          paramName:  'file', // S3 does not like nested name fields i.e. name="user[avatar_url]"
+          dataType: 'XML',  // S3 returns XML if success_action_status is set to 201
+          replaceFileInput: false,
+          progressall: function (e, data) {
+              var progress = parseInt(data.loaded / data.total * 100, 10);
+              progressBar.css('width', progress + '%')
+          },
+          start: function (e) {
+              submitButton.prop('disabled', true);
+              progressBar.
+                css('background', 'green').
+                css('display', 'block').
+                css('width', '0%').
+                text("Loading...");
+          },
+          done: function(e, data) {
+              submitButton.prop('disabled', false);
+              progressBar.text("Uploading done");
+
+              // extract key and generate URL from response
+              var key   = $(data.jqXHR.responseXML).find("Key").text();
+              var url   = '//' + form.data('host') + '/' + key;
+
+              // create hidden field
+              var input = $("<input />", { type:'input', name: fileInput.attr('name'), value: url })
+              form.append(input);
+          },
+          fail: function(e, data) {
+              submitButton.prop('disabled', false);
+              progressBar.
+                css("background", "red").
+                text("Failed");
+          }
+      });
     });
   });
-})
+});
