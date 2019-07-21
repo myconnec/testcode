d7140f06f3329147b7d15919398d261a08eb9263
davidjeddy <me@davidjeddy.com>
2019-07-21 18:34:44 -0400

    Upload on button press and progress bar now funcitonal.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -127,7 +127,7 @@ class ListingsController < ApplicationController
 
   def set_s3_direct_post
     @s3_direct_post = S3_BUCKET.presigned_post(
-      key: "#{SecureRandom.uuid}_#{SecureRandom.uuid}",
+      key: "#{SecureRandom.uuid}_${filename}",
       success_action_status: '201'
     )
   end
