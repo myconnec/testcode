80a4830947a3923f400329775632ea80c7fbcd33
davidjeddy <me@davidjeddy.com>
2019-08-15 18:31:22 -0400

    TF init completes successfully.

--- terraform/lambda_s3_to_email/s3.tf
+++ terraform/lambda_s3_to_email/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = "${var.media_display_bucket_id}"
+  bucket = "${var.video_process_media_display_bucket_arn}"
 
   lambda_function {
     lambda_function_arn = "${aws_lambda_function.media_processing_complete_email.arn}"
