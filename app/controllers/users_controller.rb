4e3a46f8beffd3c81eb818902e51ae8bfebf87f4
davidjeddy <me@davidjeddy.com>
2019-08-25 00:22:07 -0400

    FIXED formatting of app UserCNTL.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
-    def show
-      @user = User.find_by username: params[:username]
-      
-      @listings = Listing
-        .where(user_id: @user.id)
-        .where("ending_at > '#{Time.now.to_i}'")
-        .where("media_file_name IS NOT NULL")
-        .order("created_at DESC")
+  def show
+    @user = User.find_by username: params[:username]
 
-        signer = Aws::S3::Presigner.new
-        @listings.each do | listing |    
-          listing.presigned_media_url = signer.presigned_url(
-            :get_object,
-            bucket: ENV['AWS_S3_MEDIA_DISPLAY_BUCKET'],
-            key: listing.media_file_name
-          )
-        end
+    @listings = Listing
+      .where(user_id: @user.id)
+      .where("ending_at > '#{Time.now.to_i}'")
+      .where("media_file_name IS NOT NULL")
+      .order("created_at DESC")
+
+    signer = Aws::S3::Presigner.new
+    @listings.each do | listing |
+      listing.presigned_media_url = signer.presigned_url(
+        :get_object,
+        bucket: ENV['AWS_S3_MEDIA_DISPLAY_BUCKET'],
+        key: listing.media_file_name
+      )
     end
+  end
 end
