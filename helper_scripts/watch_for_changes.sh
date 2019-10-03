921684403ea422be6358856041fa9876cc571b5e
davidjeddy <me@davidjeddy.com>
2019-10-03 19:51:29 -0400

    UPDATED helper script.

--- helper_scripts/watch_for_changes.sh
+++ helper_scripts/watch_for_changes.sh
@@ -1,14 +1,11 @@
 #!/bin/bash -xe
 
-echo "Now we are watching the ./app dir for changes..."
-# fswatch -0 -o ./app | \
-# xargs -0 -I {} \
-# rsync -avc --delete --progress -e "ssh -i ~/.ssh/$(terraform output AWS_PEM_KEY_PAIR).pem" \
-# ./app/ \
-# ubuntu@$(terraform output aws_instance_web_app_dns):/home/ubuntu/connechub/app
+echo "Syncing local file to remote..."
 
-fswatch -0 -o -e .git/ -e .pyc -e $(pwd)/static . | xargs -0 -I {} rsync -avzP -e "ssh -i ~/.ssh/$(terraform output AWS_PEM_KEY_PAIR).pem" \
+# fswatch -0 -o -e .git/ -e .pyc -e $(pwd)/static . | xargs -0 -I {}
+
+rsync -avzP -e "ssh -i ~/.ssh/$(terraform output AWS_PEM_KEY_PAIR).pem" \
 ./app \
 ubuntu@$(terraform output aws_instance_web_app_dns):/home/ubuntu/connechub/app
 
-
+echo "...done."
