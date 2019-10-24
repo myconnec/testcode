3097c722fa38b1b7e1200b863478a64195f43153
davidjeddy <me@davidjeddy.com>
2019-10-23 23:17:23 -0400

    FIX for module.lambda_s3_to_email.aws_s3_bucket_notification.display_bucket_notification (destroy): 1 error occurred: error.

--- terraform/lambda_s3_to_email/s3.tf
+++ terraform/lambda_s3_to_email/s3.tf
@@ -1,6 +1,10 @@
 resource "aws_s3_bucket_notification" "display_bucket_notification" {
   bucket = "${var.media_display_bucket_id}"
 
+  depends_on = [
+    "${var.media_display_bucket_id.id}"
+  ]
+
   lambda_function {
     lambda_function_arn = "${aws_lambda_function.media_processing_complete_email.arn}"
     events              = ["s3:ObjectCreated:*"]
