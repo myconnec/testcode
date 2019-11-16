cdfdf3c60a24dd908808861b80dd3d8ec697e84f
davidjeddy <me@davidjeddy.com>
2019-11-16 15:16:35 -0500

    FIXED thumbnail img display issue post media extension handling change.

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -7,12 +7,14 @@ class SubcategoriesController < ApplicationController
             .where("media_file_name IS NOT NULL")
             .order("created_at DESC")
 
+            # TODO move this into a CNTL helper
+            # media_file_name, add -00001.png
             signer = Aws::S3::Presigner.new
             @listings.each do | listing |    
                 listing.presigned_media_url = signer.presigned_url(
                   :get_object,
                   bucket: ENV['AWS_S3_MEDIA_DISPLAY_BUCKET'],
-                  key: (listing.media_file_name[0..-5] + '-00001.png')
+                  key: (listing.media_file_name[0..-5] + '.mp4-00001.png')
                 )
             end
 
