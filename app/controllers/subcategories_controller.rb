05ca4bbdfd96057055edeeace008faf8496246ee
davidjeddy <me@davidjeddy.com>
2019-11-24 08:35:20 -0500

    Almost, now only need sub_cat VW to work.

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,6 +1,10 @@
 class SubcategoriesController < ApplicationController
 
     def show
+        @all_categories = Category.where('1=1')
+
+        @category = Category.find(params[:category_id])
+
         @listings = Listing
             .where(subcategory_id: params[:id])
             .where("ending_at > '#{Time.now.to_i}'")
@@ -10,7 +14,7 @@ class SubcategoriesController < ApplicationController
         # TODO move this to a CNTL helper
         # media_file_name, add -00001.png
         signer = Aws::S3::Presigner.new
-        @listings.each do | listing |    
+        @listings.each do | listing |
             listing.presigned_media_url = signer.presigned_url(
                 :get_object,
                 bucket: ENV['AWS_S3_MEDIA_DISPLAY_BUCKET'],
@@ -18,13 +22,12 @@ class SubcategoriesController < ApplicationController
             )
         end
 
-        @category = Category.find(params[:category_id])
         @subcategory = Subcategory.find(params[:id])
     end
 
     def find_by_category
         category = Category.find(params[:category_id])
-        subcategories = category.subcategories.find_all
+        subcategories = Subcategory.where('1=1')
         render json: { subcategories: subcategories }
     end
 end
