60f84d9067b517e083dc320e1aa6098536c2ab9d
davidjeddy <me@davidjeddy.com>
2019-11-24 00:24:23 -0500

    Cont re-working Listings update_upload().

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -100,8 +100,8 @@ class ListingsController < ApplicationController
     # So, we have to do a reverse count to the last `.` and truncate the string at that lingth.
     # replace ANY file extension with .mp4, that is the ONLY output format we provide
     file_name = params[:media_file_name]
-    file_name = file_name[0...(file_name.length - file_name.reverse.index('.'))] + 'mp4'.downcase
-    file_name = file_name.gsub!(/[^0-9a-zA-Z\-\.\/]/, '')
+    file_name = file_name[0...(file_name.length - file_name.reverse.index('.'))] + 'mp4'
+    file_name = file_name.downcase.gsub!(/[^0-9a-zA-Z\-\.\/]/, '')
 
     # check file extention is of allowed format
     file_ext = file_name[(file_name.length - file_name.reverse.index('.'))...file_name.length].downcase
