c3ce335d17045f7d5cab197ec5b361fc352b7792
David J Eddy <me@davidjeddy.com>
2019-12-08 19:54:44 -0500

    Useds git bisect to find the commit that broke updating. Reverted CNTL() security changes.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -136,7 +136,6 @@ class ListingsController < ApplicationController
 
     @comments = Comment.where(listing_id: @listing).order("created_at DESC")
     @user = current_user
-    @user = current_user
 
     # source https://stackoverflow.com/questions/44741473/recommended-way-to-generate-a-presigned-url-to-s3-bucket-in-ruby
     # source https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/S3/Object.html#presigned_url-instance_method
