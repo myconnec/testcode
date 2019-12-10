9ee6adcb86ee7dc92c0fa40d714fb6e40345389a
David J Eddy <me@davidjeddy.com>
2019-12-10 18:59:20 -0500

    Final fixes for UAT tests suite.

--- cypress/integration/connechub/reset_password.js
+++ cypress/integration/connechub/reset_password.js
@@ -10,26 +10,28 @@ describe('User Management functionality.', function() {
     it('...reset password on website.', function() {
         cy.login()
         cy.view_user_profile()
-        cy.get('#navbar > ul > li.dropdown.open > ul > li:nth-child(2) > a').contains('Edit Your Account').should('be.visible').click()
-        cy.get('body > div:nth-child(8) > div > div > div > div.panel-heading > h2').contains('Edit Your Account').click()
+
+        cy.get('#inside_view_left > div:nth-child(7) > a').contains('Edit Your Profile').click()
 
         cy.get('#user_current_password').clear().type('testtest')
         cy.get('#user_password').clear().type(userData.password)
         cy.get('#user_password_confirmation').clear().type(userData.password)
         cy.get('#edit_user > div.form-group > input').click()
+
         cy.logout()
     })
 
     it('...confirm new password works.', function() {
         cy.login(userData)
         cy.view_user_profile()
-        cy.get('#navbar > ul > li.dropdown.open > ul > li:nth-child(2) > a').contains('Edit Your Account').should('be.visible').click()
-        cy.get('body > div:nth-child(8) > div > div > div > div.panel-heading > h2').contains('Edit Your Account').click()
+
+        cy.get('#inside_view_left > div:nth-child(7) > a').contains('Edit Your Profile').click()
 
         cy.get('#user_current_password').clear().type(userData.password)
         cy.get('#user_password').clear().type('testtest')
         cy.get('#user_password_confirmation').clear().type('testtest')
         cy.get('#edit_user > div.form-group > input').click()
+
         cy.logout()
     })
 })
