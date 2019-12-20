cbb70618c00dc73d2538f7bcaf977332ae40efc1
davidjeddy <me@davidjeddy.com>
2019-12-20 00:15:27 -0500

    REMOVED infra. resources.

--- helper_scripts/docs/toolchain.md
+++
@@ -1,8 +0,0 @@
-# Toolchain
-
-- [Ansible](https://www.ansible.com/)
-- [AWS CLI](https://aws.amazon.com)
-- [Terraform](https://app.terraform.io/app/ConnecHub/workspaces)
-- [Ruby on Rails](https://rubyonrails.org/)
-- [Puma](https://github.com/puma/puma) webserver
-
--- helper_scripts/ssh_dev_web_app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-dev_branch=$(git rev-parse --abbrev-ref HEAD)
-
-ssh -i ~/.ssh/$(terraform output AWS_PEM_KEY_PAIR).pem ubuntu@$(terraform output aws_instance_web_app_dns) "\
-cd ./connechub \
-git checkout $dev_branch --force && git pull origin"
-
-ssh -i ~/.ssh/$(terraform output AWS_PEM_KEY_PAIR).pem ubuntu@$(terraform output aws_instance_web_app_dns)
