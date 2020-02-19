16a711fc90626453db224d24ead0e3a958d7b4ec
davidjeddy <me@davidjeddy.com>
2020-02-18 19:45:03 -0500

    UPDATED ./db/seed.rb from .production! to .prd! to match infra values.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 # Source: https://gist.github.com/seyhunak/7843549
-unless Rails.env.production?
-    connection = ActiveRecord::Base.connection
-    connection.tables.each do |table|
-      connection.execute("TRUNCATE #{table}") unless table == "schema_migrations"
-    end
+unless Rails.env.prd?
+  connection = ActiveRecord::Base.connection
+  connection.tables.each do |table|
+    connection.execute("TRUNCATE #{table}") unless table == "schema_migrations"
+  end
 
-    sql = File.read('./db/sql/database.sql')
-    statements = sql.split(/;$/)
-    statements.pop
+  sql = File.read('./db/sql/database.sql')
+  statements = sql.split(/;$/)
+  statements.pop
 
-    ActiveRecord::Base.transaction do
-      statements.each do |statement|
-        connection.execute(statement)
-      end
+  ActiveRecord::Base.transaction do
+    statements.each do |statement|
+      connection.execute(statement)
     end
   end
+end
