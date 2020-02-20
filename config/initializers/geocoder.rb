1c4b76002ba2d03084752b9ba9f271671f8f66e5
davidjeddy <me@davidjeddy.com>
2020-02-20 18:26:39 -0500

    Changed Geolocation API provider to geocodio.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,9 +1,9 @@
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
-  lookup: :google,              # name of geocoding service (symbol)
+  lookup: :geocodio,              # name of geocoding service (symbol)
   # ip_lookup: :freegeoip,      # name of IP address geocoding service (symbol)
-  # language: :en,              # ISO-639 language code
+  language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
