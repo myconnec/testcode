95269a95e43b8116478f7166c3708b6ef68b0944
davidjeddy <me@davidjeddy.com>
2020-02-28 11:39:51 -0600

    Created logic to allow  to execute only if no  items exist.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,22 @@
 # Source: https://gist.github.com/seyhunak/7843549
 # DO NOT seed in RAILS production ENVs
 unless Rails.env.production?
-  connection = ActiveRecord::Base.connection
-  connection.tables.each do |table|
-    connection.execute("TRUNCATE #{table}") unless table == "schema_migrations"
-  end
+  # DB DUMP DROPS TABLES
+  # connection = ActiveRecord::Base.connection
+  # connection.tables.each do |table|
+  #   connection.execute("TRUNCATE #{table}") unless table == "schema_migrations"
+  # end
 
-  sql = File.read('./db/sql/database.sql')
-  statements = sql.split(/;$/)
-  statements.pop
+  results = ActiveRecord::Base.connection.execute("SELECT count(*) FROM categories;")
+  if results == 0
+    sql = File.read('./db/sql/database.sql')
+    statements = sql.split(/;$/)
+    statements.pop
 
-  ActiveRecord::Base.transaction do
-    statements.each do |statement|
-      connection.execute(statement)
+    ActiveRecord::Base.transaction do
+      statements.each do |statement|
+        connection.execute(statement)
+      end
     end
   end
 end
