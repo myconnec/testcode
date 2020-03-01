6259a217f109e539e55b32b11f1fe238bca7545f
davidjeddy <me@davidjeddy.com>
2020-03-01 17:30:15 -0500

    UPDATED Db:seed logic to allow seeding if the categories table is empty.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,13 @@
-# Source: https://gist.github.com/seyhunak/7843549
-# DO NOT seed in RAILS production ENVs
-unless Rails.env.production?
-  # DB DUMP DROPS TABLES
-  # connection = ActiveRecord::Base.connection
-  # connection.tables.each do |table|
-  #   connection.execute("TRUNCATE #{table}") unless table == "schema_migrations"
-  # end
+# if not data in database, allow seeding
+results = ActiveRecord::Base.connection.execute("SELECT count(*) FROM categories;")
+if results == 0
+  sql = File.read('./db/sql/database.sql')
+  statements = sql.split(/;$/)
+  statements.pop
 
-  results = ActiveRecord::Base.connection.execute("SELECT count(*) FROM categories;")
-  if results == 0
-    sql = File.read('./db/sql/database.sql')
-    statements = sql.split(/;$/)
-    statements.pop
-
-    ActiveRecord::Base.transaction do
-      statements.each do |statement|
-        connection.execute(statement)
-      end
+  ActiveRecord::Base.transaction do
+    statements.each do |statement|
+      connection.execute(statement)
     end
   end
 end
