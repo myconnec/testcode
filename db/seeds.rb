88491a9490c23425aded2b4ce961608fd72383b3
davidjeddy <me@davidjeddy.com>
2020-03-01 22:30:23 -0500

    DISABLED  for now.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-# if not data in database, allow seeding
-results = ActiveRecord::Base.connection.execute("SELECT count(*) FROM categories;")
-if results == 0
-  sql = File.read('./db/sql/database.sql')
-  statements = sql.split(/;$/)
-  statements.pop
+# # if not data in database, allow seeding
+# results = ActiveRecord::Base.connection.execute("SELECT count(*) FROM categories;")
+# if results == 0
+#   sql = File.read('./db/sql/database.sql')
+#   statements = sql.split(/;$/)
+#   statements.pop
 
-  ActiveRecord::Base.transaction do
-    statements.each do |statement|
-      connection.execute(statement)
-    end
-  end
-end
+#   ActiveRecord::Base.transaction do
+#     statements.each do |statement|
+#       connection.execute(statement)
+#     end
+#   end
+# end
