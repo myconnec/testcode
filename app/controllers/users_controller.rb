1684fbc462a63ad7b7b767410d64bf097b89de60
davidjeddy <me@davidjeddy.com>
2020-03-04 16:45:06 -0500

    UPDATED ./app/controllers/users_controller.rb Devise filters. UPDATED user profile edit form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def update_resource(resource, params)
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+      params.delete(:current_password)
+      resource.update_without_password(params)
+    else
+      super
+    end
+  end
+
   def catch_not_found
     yield
     rescue
