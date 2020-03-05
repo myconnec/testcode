dbff9ff5bfc8dcd826039b2672f0b5b51c2515a1
davidjeddy <me@davidjeddy.com>
2020-03-04 21:44:05 -0500

    UPDATED DB seed logic.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 tmp = ssm_client.get_parameter(name: '/stage', with_decryption: true).to_h[:parameter][:value].to_s
-if tmp != 'prd'
+if tmp == 'dev'
+  connection = ActiveRecord::Base.connection
+  connection.tables.each do |table|
+    connection.execute("TRUNCATE #{table}") unless table == "schema_migrations"
+  end
+
   sql = File.read('./db/sql/database.sql')
   statements = sql.split(/;$/)
   statements.pop
