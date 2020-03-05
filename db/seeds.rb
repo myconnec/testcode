786a20b98062b8e0d5317a89d470df42756d3030
davidjeddy <me@davidjeddy.com>
2020-03-04 21:41:26 -0500

    ADDED logic to allow rake db:seed if the ENV is dev.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
-# # if not data in database, allow seeding
-# results = ActiveRecord::Base.connection.execute("SELECT count(*) FROM categories;")
-# if results == 0
-#   sql = File.read('./db/sql/database.sql')
-#   statements = sql.split(/;$/)
-#   statements.pop
+tmp = ssm_client.get_parameter(name: '/stage', with_decryption: true).to_h[:parameter][:value].to_s
+if tmp != 'prd'
+  sql = File.read('./db/sql/database.sql')
+  statements = sql.split(/;$/)
+  statements.pop
 
-#   ActiveRecord::Base.transaction do
-#     statements.each do |statement|
-#       connection.execute(statement)
-#     end
-#   end
-# end
+  ActiveRecord::Base.transaction do
+    statements.each do |statement|
+      connection.execute(statement)
+    end
+  end
+end
