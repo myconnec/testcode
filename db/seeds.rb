0bf87af40e309cbe29af050c9132fc74fdbeb4ea
davidjeddy <me@davidjeddy.com>
2020-04-21 18:50:47 -0400

    REMOED and IGNORED ./cypress. UPDATED ./db/seed.rb in hopes of getting the seed logic to work once again.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,15 @@
-tmp = ssm_client.get_parameter(name: '/stage', with_decryption: true).to_h[:parameter][:value].to_s
-if tmp == 'dev'
-  connection = ActiveRecord::Base.connection
-  connection.tables.each do |table|
-    connection.execute("TRUNCATE #{table}") unless table == "schema_migrations"
-  end
+connection = ActiveRecord::Base.connection
 
-  sql = File.read('./db/sql/database.sql')
-  statements = sql.split(/;$/)
-  statements.pop
+connection.tables.each do |table|
+  connection.execute("TRUNCATE #{table}") unless table == "schema_migrations"
+end
 
-  ActiveRecord::Base.transaction do
-    statements.each do |statement|
-      connection.execute(statement)
-    end
+sql = File.read('./db/sql/database.sql')
+statements = sql.split(/;$/)
+statements.pop
+
+ActiveRecord::Base.transaction do
+  statements.each do |statement|
+    connection.execute(statement)
   end
 end
