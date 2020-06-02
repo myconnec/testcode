f7490c62bb67c59c7b93c0947a164058605a3fd2
David J Eddy <david@connechub.com>
2020-06-02 17:32:30 -0400

    REMOVED directory listing from landing page. REMOVED directory listing related images. REMOVED unused download.png image.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,34 +1,6 @@
 class CategoriesController < ApplicationController
 
     def index
-        @categories = Category.all
-
-        @campus = @categories[0]
-        @campus_subcat_posting_count = Subcategory.count_per_subcat(@campus)
-
-        @community = @categories[1]
-        @community_subcat_posting_count = Subcategory.count_per_subcat(@community)
-
-        @housing = @categories[2]
-        @housing_subcat_posting_count = Subcategory.count_per_subcat(@housing)
-
-        @h2f = @categories[3]
-        @h2f_subcat_posting_count = Subcategory.count_per_subcat(@h2f)
-
-        @free = @categories[4]
-        @free_subcat_posting_count = Subcategory.count_per_subcat(@free)
-
-        @jobs = @categories[5]
-        @jobs_subcat_posting_count = Subcategory.count_per_subcat(@jobs)
-
-        @sale = @categories[6]
-        @sale_subcat_posting_count = Subcategory.count_per_subcat(@sale)
-
-        @services = @categories[7]
-        @services_subcat_posting_count = Subcategory.count_per_subcat(@services)
-
-        @unboxing = @categories[8]
-        @unboxing_subcat_posting_count = Subcategory.count_per_subcat(@unboxing)
     end
 
     def show
