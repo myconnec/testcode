07965d5226fe7f3a0685a6b88488257a81570415
David J Eddy <david@connechub.com>
2020-06-06 12:26:02 -0400

    ADDED placeholder 1.1.10 UAT test file.

---
+++ cypress/integration/connechub/1.1.10.js
@@ -0,0 +1,5 @@
+/**
+ * Release 1.1.10 changes
+ * 
+ * This release involved a major change to the UI; the sub/caregory meny was moved and iterated on as well as the landing page changed. These changes are reflected throughout the Cypress e2e test suite, not simply in here.
+ */
