c7548511c91600675972ff5425265669f2f89c28
David J Eddy <david@connechub.com>
2020-06-06 12:53:31 -0400

    ADDED path indicator to show appropriate quality thumbnail based on context.

--- cypress/integration/connechub/1.1.10.js
+++ cypress/integration/connechub/1.1.10.js
@@ -3,3 +3,34 @@
  * 
  * This release involved a major change to the UI; the sub/caregory meny was moved and iterated on as well as the landing page changed. These changes are reflected throughout the Cypress e2e test suite, not simply in here.
  */
+
+// FIX Stripe value source. Add Listing PAID crud UAT testing.
+// https://trello.com/c/U6pfaJSh/49-fix-stripe-value-source-add-listing-paid-crud-uat-testing
+// see listing_crud_paid.js
+
+// ADD UpTime Robot
+// https://trello.com/c/rFKdFu3r/48-add-uptime-robot
+// not in scope of UAT testing
+
+// We need something on the home page to tell users who we are.
+// https://trello.com/c/gWcKTaQG/32-we-need-something-on-the-home-page-to-tell-users-who-we-are
+// screenshot specifically of the header image
+
+// Facebook Open Graph Meta Tags
+// https://trello.com/c/sr1jg9dq/31-facebook-open-graph-meta-tags
+// create listing, goto FB Share debugger, submit listing, screen capture result
+
+// Website Redesign (www.connechub.com)
+// https://trello.com/c/zgNPmX3D/38-website-redesign-wwwconnechubcom
+// sub/category change, landing page layout change
+
+// Add another subcategory called "Auto Parts" this will go under the "Sale" category
+// https://trello.com/c/M7rBsihc/28-add-another-subcategory-called-auto-parts-this-will-go-under-the-sale-category
+
+// Update Cypress UAT tests for better performance
+// https://trello.com/c/tqchBOgA/41-update-cypress-uat-tests-for-better-performance
+// done throughout test suite
+
+// Google Analytics is not recording session data
+// https://trello.com/c/7JNtSTkZ/29-google-analytics-is-not-recording-session-data
+// not in scope of UAT testing
