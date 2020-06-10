affdae208798b05f8744575880edbb939b63232b
David J Eddy <david@connechub.com>
2020-06-10 19:57:03 -0400

    NORMALIZED config/environments/*.rb dev and prd configs.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,14 +6,14 @@ Rails.application.configure do
     # since you don't have to restart the web server when you make code changes.
     config.cache_classes = false
 
-    # Paperclip
-    Paperclip.options[:command_path] = "/usr/local/bin/ffmpeg"
-
-    # Do not eager load code on boot.
+    # Eager load code on boot. This eager loads most of Rails and
+    # your application in memory, allowing both threaded web servers
+    # and those relying on copy on write to perform better.
+    # Rake tasks automatically ignore this option for performance.
     config.eager_load = false
 
     # Show full error reports and disable caching.
-    config.consider_all_requests_local = true
+    config.consider_all_requests_local       = true
     config.action_controller.perform_caching = false
 
     # Raises error for missing translations
@@ -28,7 +28,7 @@ Rails.application.configure do
     # Debug mode disables concatenation and preprocessing of assets.
     # This option may cause significant delays in view rendering with a large
     # number of complex assets.
-    config.assets.debug = false
+    config.assets.debug = true
 
     # Asset digests allow you to set far-future HTTP expiration dates on all assets,
     # yet still be able to expire them through the digest params.
