26eff119b19b94ed5fd5b9d1b5a1e6d3b0e33aba
David J Eddy <david@connechub.com>
2020-06-23 20:39:25 -0400

    FIXED https://trello.com/c/bU835RXS/59-forgot-password-form-submit-error. UPDATED package.json and package-lock.json

--- cypress/integration/connechub/1.1.11.js
+++ cypress/integration/connechub/1.1.11.js
@@ -1,13 +1,26 @@
 /**
  * Release 1.1.11 changes
  */
-describe('Release 1.1.10 changes ...', function () {
+describe('Release 1.1.11 changes ...', function () {
     
     const listingData = []
-    const userData = []
+    const userData = [
+        {
+            name: 'Test User',
+            email: 'test@connechub.com',
+            password: 'testtest'
+        }
+    ]
 
     // Forgot password form submit error
     // https://trello.com/c/bU835RXS/59-forgot-password-form-submit-error
     it('...forgot password form submit error.', function () {
+        cy.visit('/users/sign_in')
+        cy.contains('Forgot your password?').click()
+
+        cy.get('#user_email').clear().type(userData[0]['email'])
+        cy.contains('Reset Your Password').click()
+
+        cy.handle_splash_message('You will receive an email with instructions on how to reset your password in a few minutes.', 'success')
     })
 })
