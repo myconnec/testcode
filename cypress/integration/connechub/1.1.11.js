37bf0892b1d3c994846117ff139f784af847c6fa
David J Eddy <david@connechub.com>
2020-06-23 21:10:39 -0400

    UPDATED About Us view per https://trello.com/c/6674txFF/33-update-about-us-page-with-new-verbiage

--- cypress/integration/connechub/1.1.11.js
+++ cypress/integration/connechub/1.1.11.js
@@ -23,4 +23,19 @@ describe('Release 1.1.11 changes ...', function () {
 
         cy.handle_splash_message('You will receive an email with instructions on how to reset your password in a few minutes.', 'notice')
     })
+
+    // UPDATE About us photo.
+    it('...UPDATE About us photo.', function () {
+        cy.visit('/about')
+        cy.get('body > div:nth-child(8) > div.container > header', {timeout: 5000})
+            .matchImageSnapshot('1_1_11_update_about_us_header_image');
+    })
+
+    // UPDATE About Us page with new verbiage
+    it('...UPDATE About Us page with new verbiage.', function () {
+        cy.visit('/about')
+        cy.contains('Connechub is a local classified ad website that allows users to post their video content; to list a job, promote your business, unbox the latest item, or sell your stuff.')
+        cy.contains('With Connechub you can post your video content from any source such as; your cellphone (Android, IOS), DLSR camera, Mirrorless Camera, Video camera and any other video source.')
+        cy.contains('As the old cliche goes, "A picture is worth a thousand words". However, "a video is worth a million more".');
+    })
 })
