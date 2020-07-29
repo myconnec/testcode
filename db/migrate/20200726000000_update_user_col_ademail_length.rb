1364adb53982176e33c8de27f5d2f8dd40ed7948
David J Eddy <david@connechub.com>
2020-07-28 22:28:15 -0400

    FIXED migration, wrong column name. :S

--- db/migrate/20200726000000_update_user_col_ademail_length.rb
+++ db/migrate/20200726000000_update_user_col_ademail_length.rb
@@ -1,9 +1,9 @@
-class UpdateUserColAdemailLength < ActiveRecord::Migration
+class UpdateUserColEmailLength < ActiveRecord::Migration
   def up
-    change_column :users, :ademail, :string, :limit => 256
+    change_column :users, :email, :string, :limit => 256
   end
 
   def down
-    change_column :users, :ademail, :string, :limit => 32
+    change_column :users, :email, :string, :limit => 32
   end
 end
