5dd7654509008f0ec3d78717bffa1c3a9a776233
David J Eddy <david@connechub.com>
2020-07-28 22:23:00 -0400

    FIXED migration, the table is users not user.

--- db/migrate/20200726000000_update_user_col_ademail_length.rb
+++ db/migrate/20200726000000_update_user_col_ademail_length.rb
@@ -1,9 +1,9 @@
 class UpdateUserColAdemailLength < ActiveRecord::Migration
   def up
-    change_column :user, :ademail, :string, :limit => 256
+    change_column :users, :ademail, :string, :limit => 256
   end
 
   def down
-    change_column :user, :ademail, :string, :limit => 32
+    change_column :users, :ademail, :string, :limit => 32
   end
 end
