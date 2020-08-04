056112e11c73296ac91ce2dda155b644410a3266
David J Eddy <david@connechub.com>
2020-08-03 23:45:57 -0400

    Listing count once again displaying next to the sub category.

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -5,10 +5,10 @@ class Subcategory < ActiveRecord::Base
     validates_presence_of :category_id
     validates_presence_of :chargable
 
-    # version >= 1.1.13
-    # Use the request cat id to limit the sub_cat listing id range
-    def self.get_subcat_listing_counts(sub_category)
-        return 0
-        # subcat_listing_counts = Category.includes(:subcategory_id).map { |c| c.category_id }
+    def listing_count(category_id, subcategory_id)
+        Listing
+            .where("category_id = '#{category_id}'")
+            .where("subcategory_id = '#{subcategory_id}'")
+            .count()
     end
 end
