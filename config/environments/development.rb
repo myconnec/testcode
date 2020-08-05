697896b85aff6342ecc8f1fd93f4122687549a3c
David J Eddy <david@connechub.com>
2020-08-04 22:39:34 -0400

    CHANGED development.rb to match production.rb for the purpose of PageSpeed updates.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,17 +4,17 @@ Rails.application.configure do
     # In the development environment your application's code is reloaded on
     # every request. This slows down response time but is perfect for development
     # since you don't have to restart the web server when you make code changes.
-    config.cache_classes = false
+    config.cache_classes = true
 
     # Eager load code on boot. This eager loads most of Rails and
     # your application in memory, allowing both threaded web servers
     # and those relying on copy on write to perform better.
     # Rake tasks automatically ignore this option for performance.
-    config.eager_load = false
+    config.eager_load = true
 
     # Show full error reports and disable caching.
-    config.consider_all_requests_local       = true
-    config.action_controller.perform_caching = false
+    config.consider_all_requests_local       = false
+    config.action_controller.perform_caching = true
 
     # Raises error for missing translations
     # config.action_view.raise_on_missing_translations = true
@@ -28,7 +28,7 @@ Rails.application.configure do
     # Debug mode disables concatenation and preprocessing of assets.
     # This option may cause significant delays in view rendering with a large
     # number of complex assets.
-    config.assets.debug = true
+    config.assets.debug = false
 
     # Asset digests allow you to set far-future HTTP expiration dates on all assets,
     # yet still be able to expire them through the digest params.
@@ -37,7 +37,7 @@ Rails.application.configure do
     # Adds additional error checking when serving assets at runtime.
     # Checks for improperly declared sprockets dependencies.
     # Raises helpful error messages.
-    config.assets.raise_runtime_errors = true
+    config.assets.raise_runtime_errors = false
 
     # action_mailer for email configuration
     config.action_mailer.default_url_options = {
