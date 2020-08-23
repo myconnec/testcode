937f0b338c58275c2003f157b7559a5f107cebd4
davidjeddy <me@davidjeddy.com>
2020-08-22 21:35:27 -0400

    Misspelled chargable as chargeable

--- db/migrate/20200218194501_update_subcategories_add_more_services.rb
+++ db/migrate/20200218194501_update_subcategories_add_more_services.rb
@@ -1,25 +1,25 @@
 class UpdateSubcategoriesAddMoreServices < ActiveRecord::Migration
   def change
-    Subcategory.create :name => "tax services", :category_id => 10, :chargeable => 0
-    Subcategory.create :name => "funerals service", :category_id => 10, :chargeable => 0
-    Subcategory.create :name => "real estate investor", :category_id => 10, :chargeable => 0
-    Subcategory.create :name => "real estate agents", :category_id => 10, :chargeable => 0
-    Subcategory.create :name => "grocery stores", :category_id => 10, :chargeable => 0
-    Subcategory.create :name => "retailers", :category_id => 10, :chargeable => 0
-    Subcategory.create :name => "food trucks/mobile food", :category_id => 10, :chargeable => 0
-    Subcategory.create :name => "internet firm", :category_id => 10, :chargeable => 0
-    Subcategory.create :name => "pain specialist", :category_id => 10, :chargeable => 0
+    Subcategory.create :name => "tax services", :category_id => 10, :chargable => 0
+    Subcategory.create :name => "funerals service", :category_id => 10, :chargable => 0
+    Subcategory.create :name => "real estate investor", :category_id => 10, :chargable => 0
+    Subcategory.create :name => "real estate agents", :category_id => 10, :chargable => 0
+    Subcategory.create :name => "grocery stores", :category_id => 10, :chargable => 0
+    Subcategory.create :name => "retailers", :category_id => 10, :chargable => 0
+    Subcategory.create :name => "food trucks/mobile food", :category_id => 10, :chargable => 0
+    Subcategory.create :name => "internet firm", :category_id => 10, :chargable => 0
+    Subcategory.create :name => "pain specialist", :category_id => 10, :chargable => 0
   end
 
   def down
-    Subcategory.delete :name => "tax services", :category_id => 10, :chargeable => 0
-    Subcategory.delete :name => "funerals service", :category_id => 10, :chargeable => 0
-    Subcategory.delete :name => "real estate investor", :category_id => 10, :chargeable => 0
-    Subcategory.delete :name => "real estate agents", :category_id => 10, :chargeable => 0
-    Subcategory.delete :name => "grocery stores", :category_id => 10, :chargeable => 0
-    Subcategory.delete :name => "retailers", :category_id => 10, :chargeable => 0
-    Subcategory.delete :name => "food trucks/mobile food", :category_id => 10, :chargeable => 0
-    Subcategory.delete :name => "internet firm", :category_id => 10, :chargeable => 0
-    Subcategory.delete :name => "pain specialist", :category_id => 10, :chargeable => 0
+    Subcategory.delete :name => "tax services", :category_id => 10, :chargable => 0
+    Subcategory.delete :name => "funerals service", :category_id => 10, :chargable => 0
+    Subcategory.delete :name => "real estate investor", :category_id => 10, :chargable => 0
+    Subcategory.delete :name => "real estate agents", :category_id => 10, :chargable => 0
+    Subcategory.delete :name => "grocery stores", :category_id => 10, :chargable => 0
+    Subcategory.delete :name => "retailers", :category_id => 10, :chargable => 0
+    Subcategory.delete :name => "food trucks/mobile food", :category_id => 10, :chargable => 0
+    Subcategory.delete :name => "internet firm", :category_id => 10, :chargable => 0
+    Subcategory.delete :name => "pain specialist", :category_id => 10, :chargable => 0
   end
 end
