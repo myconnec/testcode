e984f9c29cfcf1b9028302de1da39fa72fa63350
davidjeddy <me@davidjeddy.com>
2020-08-22 22:13:00 -0400

    FIXED migrations. Now empty to current executes successfully.

--- db/migrate/20200525165500_update_subcategories_add_auto_parts.rb
+++ db/migrate/20200525165500_update_subcategories_add_auto_parts.rb
@@ -1,9 +1,12 @@
 class UpdateSubcategoriesAddAutoParts < ActiveRecord::Migration
   def change
-    Subcategory.create :name => "Auto Parts", :category_id => 9, :chargable => 0
+    # Caused by: NoMethodError: undefined method `chargable=' for #<Subcategory id: nil, name: "tax services", category_id: 10>
+    # Subcategory.create :name => "Auto Parts", :category_id => 9, :chargable => 0
+    Subcategory.connection.execute("INSERT INTO subcategories (name, category_id, chargable) VALUES('Auto Parts', 10, 0);")
+
   end
 
   def down
-    Subcategory.delete :name => "Auto Parts", :category_id => 9
+    Subcategory.delete :name => "Auto Parts"
   end
 end
