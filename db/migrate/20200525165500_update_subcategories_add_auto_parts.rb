cc342811c167473290152a64a14eb29994b7b39e
davidjeddy <me@davidjeddy.com>
2020-08-22 21:32:55 -0400

    REMOVED ./db/seed.rb and ./db/sql/database.sql. UPDATED migration 20200218194501_update_subcategories_add_more_services.rb and 20200525165500_update_subcategories_add_auto_parts.rb to include chargeable defualt price of 0. RENAME listings new.erb to new.html.erb.

--- db/migrate/20200525165500_update_subcategories_add_auto_parts.rb
+++ db/migrate/20200525165500_update_subcategories_add_auto_parts.rb
@@ -3,7 +3,6 @@ class UpdateSubcategoriesAddAutoParts < ActiveRecord::Migration
     # Caused by: NoMethodError: undefined method `chargable=' for #<Subcategory id: nil, name: "tax services", category_id: 10>
     # Subcategory.create :name => "Auto Parts", :category_id => 9, :chargable => 0
     Subcategory.connection.execute("INSERT INTO subcategories (name, category_id, chargable) VALUES('Auto Parts', 10, 0);")
-
   end
 
   def down
