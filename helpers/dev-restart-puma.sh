3ad66a43202548a421ff823668d231d573dd7841
davidjeddy <me@davidjeddy.com>
2020-08-22 22:46:38 -0400

    ADDED TRIX rich text editor per https://github.com/maclover7/trix

---
+++ helpers/dev-restart-puma.sh
@@ -0,0 +1,17 @@
+#!/bin/bhas
+
+echo 'Run these commands on the remote host...'
+
+export RACK_ENV=development
+export RAILS_ENV=development
+
+echo '...killing old puma'
+sudo pkill -9 -f puma
+
+echo '...removing ./tmp/cache/...'
+sudo rm -rf ./tmp/cache/
+
+echo '...restart Puma...'
+bundle exec puma --bind tcp://0.0.0.0:9293 --environment ${RAILS_ENV}
+
+echo '...done.'
