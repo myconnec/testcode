2fb8f4ef52e5f4245f1d928e9818684dbbe5221f
davidjeddy <me@davidjeddy.com>
2020-09-07 15:45:03 -0400

    UPDATED MVC resource files with correct foramtting. Only whitespace change in this commit.

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,15 +1,14 @@
 class SubcategoriesController < ApplicationController
+  def show
+    @categories = Category.all
+    @category = Category.find(params[:category_id])
+    @listings = Listing.active(params[:id], "subcategory_id")
+    @subcategory = Subcategory.find(params[:id])
+  end
 
-    def show
-        @categories = Category.all
-        @category = Category.find(params[:category_id])
-        @listings = Listing.active(params[:id], 'subcategory_id')
-        @subcategory = Subcategory.find(params[:id])
-    end
-
-    def find_by_category
-        render json: {
-            subcategories: Subcategory.where(category_id: params[:category_id])
-        }
-    end
+  def find_by_category
+    render json: {
+             subcategories: Subcategory.where(category_id: params[:category_id]),
+           }
+  end
 end
