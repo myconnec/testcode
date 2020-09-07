dd6008f21c66a63570e23b40b23f20d1b7810fdc
davidjeddy <me@davidjeddy.com>
2020-09-07 15:30:14 -0400

    UPDATED helpers/auto_sync.sh. UPDATED application styles. UPDATED development.rb, reverting to deve configuration.

--- cypress/disabled/audit.js
+++ cypress/disabled/audit.js
@@ -1,5 +1,7 @@
 describe('Performance Auditing...', function () {
 
+  // TODO Only run UI/UX tests in ENVs > DEV.
+
   // TODO Cal we do these in a loop? IE for panding page, cat/subcat/listing vw'  s?
   // Landing page
   it("...Lightouse", function () {
--- helpers/auto_sync.sh
+++ helpers/auto_sync.sh
@@ -7,7 +7,6 @@
 
 export SSH_KEY=$1 #"~/.ssh/aws-connechub-dev.pem"
 export REMOTE_HOST=$2 #"18.144.166.69"
-export DIR=$3 # app || config || db || public
 
 if [[ ! -d $HOME/.ssh/ctl/ ]];then
     mkdir $HOME/.ssh/ctl/
@@ -21,33 +20,42 @@ ssh -i ${1} -nNf -o ControlMaster=yes -o ControlPath="$HOME/.ssh/ctl/%L-%r@%h:%p
 echo "Stating sync, waiting ControlPath..."
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        # Linux distros
-        echo "$OSTYPE detected..."
-        while inotifywait -r -e modify,create,delete,move $DIR; do
-            rsync -avz -e "ssh -i ${1} -o 'ControlPath=$HOME/.ssh/ctl/%L-%r@%h:%p'" $DIR ubuntu@${2}:/home/ubuntu/connechub
-            echo "...synce completed."
-        done
+    # Linux distros
+    echo "$OSTYPE detected..."
+    while inotifywait -r -e modify,create,delete,move .; do
+        rsync \
+        -avz \
+        --exclude=".git" \
+        --exclude="node_modules" \
+        -e "ssh -i ${1} -o 'ControlPath=$HOME/.ssh/ctl/%L-%r@%h:%p'" . ubuntu@${2}:/home/ubuntu/connechub
+        echo "...synce completed."
+    done
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
-        echo "$OSTYPE detected..."
-        fswatch -o $DIR | xargs -n1 -I{} \
-            rsync -avz -e "ssh -i ${1} -o 'ControlPath=$HOME/.ssh/ctl/%L-%r@%h:%p'" $DIR ubuntu@${2}:/home/ubuntu/connechub
+    # Mac OSX
+    echo "$OSTYPE detected..."
+    fswatch -o . | xargs \
+        -n1 \
+        -I{} \
+        rsync -avz \
+        --exclude=".git" \
+        --exclude="node_modules" \
+        -e "ssh -i ${1} -o 'ControlPath=$HOME/.ssh/ctl/%L-%r@%h:%p'" . ubuntu@${2}:/home/ubuntu/connechub
         echo "...synce completed."
 elif [[ "$OSTYPE" == "cygwin" ]]; then
-        # POSIX compatibility layer and Linux environment emulation for Windows
-        echo "$OSTYPE detected..."
+    # POSIX compatibility layer and Linux environment emulation for Windows
+    echo "$OSTYPE detected..."
 elif [[ "$OSTYPE" == "msys" ]]; then
-        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-        echo "$OSTYPE detected..."
+    # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+    echo "$OSTYPE detected..."
 elif [[ "$OSTYPE" == "win32" ]]; then
-        # I'm not sure this can happen.
-        echo "$OSTYPE detected..."
+    # I'm not sure this can happen.
+    echo "$OSTYPE detected..."
 elif [[ "$OSTYPE" == "freebsd"* ]]; then
-        # ...
-        echo "$OSTYPE detected..."
+    # ...
+    echo "$OSTYPE detected..."
 else
-        # Unknown.
-        echo "$OSTYPE detected..."
+    # Unknown.
+    echo "$OSTYPE detected..."
 fi
 
-            echo "...done."
+echo "...done."
