9a8aacb46e7a1c902ba4323f9596a0c42a696edd
davidjeddy <me@davidjeddy.com>
2020-09-13 20:30:21 -0400

    UPDATED top menu. No longer collapses.

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,12 +1,12 @@
 class PasswordsController < Devise::PasswordsController
-  prepend_before_action :check_captcha, only: [:create]
+  # prepend_before_action :check_captcha, only: [:create]
 
-  private
+  # private
 
-  def check_captcha
-    unless verify_recaptcha
-      self.resource = resource_class.new
-      respond_with_navigational(resource) { render :new }
-    end
-  end
+  # def check_captcha
+  #   unless verify_recaptcha
+  #     self.resource = resource_class.new
+  #     respond_with_navigational(resource) { render :new }
+  #   end
+  # end
 end
