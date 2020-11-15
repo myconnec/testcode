e5dd308aeb49faf69d47289d73eaacbeea55638f
davidjeddy <me@davidjeddy.com>
2020-11-15 17:56:45 -0500

    ENABLE configurable SOURCE and TARGET in ./helps/auto-sync.sh

--- helpers/auto-sync.sh
+++ helpers/auto-sync.sh
@@ -12,17 +12,21 @@
 
 # install sudo apt-get install -y inotify-tools
 
-export SSH_KEY=$1     #"~/.ssh/aws-connechub-dev.pem"
-export REMOTE_HOST=$2 #"18.144.166.69"
+export SSH_KEY=$1    # string~/.ssh/aws-connechub-dev.pem
+export SOURCE=$2     # string $(pwd) is source by default
+export TARGET=$3     # ip 18.144.166.69
+
+if [[ !$3 ]];
+    then SOURCE='/home/ubuntu/connechub'
+fi
 
 if [[ ! -d $HOME/.ssh/connechub/ ]]; then
     mkdir $HOME/.ssh/connechub/
 fi
-
 export CONTROL_PATH="$HOME/.ssh/connechub/%L-%r@%h:%p"
 
 echo "Starting persistance SSH connection..."
-ssh -i ${1} -nNf -o ControlMaster=yes -o ControlPath="$HOME/.ssh/connechub/%L-%r@%h:%p" ubuntu@${2}
+ssh -i ${1} -nNf -o ControlMaster=yes -o ControlPath="$HOME/.ssh/connechub/%L-%r@%h:%p" $TARGET
 
 echo "Stating sync, waiting ControlPath..."
 
@@ -34,7 +38,18 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
             -avz \
             --exclude=".git" \
             --exclude="node_modules" \
-            -e "ssh -vvv -i ${1} -o 'ControlPath=$HOME/.ssh/connechub/%L-%r@%h:%p'" . ubuntu@${2}:/home/ubuntu/connechub
+            -e "ssh -vvv -i ${1} -o 'ControlPath=$HOME/.ssh/connechub/%L-%r@%h:%p'" . $SOURCE:$TARGET
+        echo "...synce completed."
+    done
+
+    # Linux distros
+    echo "$OSTYPE detected..."
+    while inotifywait -r -e modify,create,delete,move .; do
+        rsync \
+            -avz \
+            --exclude=".git" \
+            --exclude="node_modules" \
+            -e "ssh -vvv -i ${1} -o 'ControlPath=$HOME/.ssh/connechub/%L-%r@%h:%p'" . $SOURCE:$TARGET
         echo "...synce completed."
     done
 elif [[ "$OSTYPE" == "darwin"* ]]; then
@@ -46,7 +61,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
         rsync -avz \
         --exclude=".git" \
         --exclude="node_modules" \
-        -e "ssh -i ${1} -o 'ControlPath=$HOME/.ssh/connechub/%L-%r@%h:%p'" . ubuntu@${2}:/home/ubuntu/connechub
+        -e "ssh -i ${1} -o 'ControlPath=$HOME/.ssh/connechub/%L-%r@%h:%p'" . $SOURCE:$TARGET
     echo "...synce completed."
 elif [[ "$OSTYPE" == "cygwin" ]]; then
     # POSIX compatibility layer and Linux environment emulation for Windows
