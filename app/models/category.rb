279027402206aabe5e4ec9930aca88a46f48b00f
David J Eddy <me@davidjeddy.com>
2021-09-03 19:23:08 +0200

    Cont working...

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,11 @@ class Category < ActiveRecord::Base
   def self.get_base_categories()
     Category.where("parent_id IS null")
   end
+
+    def listing_count(category_id)
+    Listing
+      .where("category_id = '#{category_id}'")
+      .where("ending_at > '#{Time.now.to_i}'")
+      .count()
+  end
 end
