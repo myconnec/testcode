5320ae0812cfef1dd1746d288fbd79f1d10cc175
David J Eddy <me@davidjeddy.com>
2021-11-18 22:34:41 +0100

    FIXED pagination item counter

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,3 +1,3 @@
 # Optionally override some pagy default with your own in the pagy initializer
-# Pagy::DEFAULT[:items] = 10        # items per page
 require 'pagy/extras/bootstrap'
+Pagy::VARS[:items]   = 9
