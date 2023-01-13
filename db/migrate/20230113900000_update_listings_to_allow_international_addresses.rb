4255fad23443af681621d91b2d05ef2e64e3b52a
David J Eddy <me@davidjeddy.com>
2023-01-13 18:57:04 +0100

    State and ZIP are now 32 characters. ADDED Province and Postal Code to label

---
+++ db/migrate/20230113900000_update_listings_to_allow_international_addresses.rb
@@ -0,0 +1,11 @@
+class UpdateListingToAllowInternationalAddresses < ActiveRecord::Migration
+  def up
+    change_column :listings, :state, :string, :limit => 32
+    change_column :listings, :zipcode, :string, :limit => 32
+  end
+
+  def down
+    change_column :listings, :state, :string, :limit => 2
+    change_column :listings, :zipcode, :string, :limit => 5
+  end
+end
